abc36b612755bc321cb8f318284cbf67026eb94b
Eddie McNally <emcn@gmx.com>
2015-08-23 21:03:47 +0100

    Misc additions to the debug util to print out extra info

--- debug_utils/move_gen_debug_analysis.py
+++ debug_utils/move_gen_debug_analysis.py
@@ -31,17 +31,34 @@ def construct_dict_of_moves(my_list):
 
 
 k_file = read_lines_from_file("/home/eddie/kkk.kkk")
-print ("number of line in kkk.kkk = ", (str(len(k_file))))
+#print ("number of line in kkk.kkk = ", (str(len(k_file))))
 
 v_file = read_lines_from_file("/home/eddie/vvv.vvv")
-print ("number of line in kkk.kkk = ", (str(len(v_file))))
+#print ("number of line in kkk.kkk = ", (str(len(v_file))))
 
 k_dict = construct_dict_of_moves(k_file)
-print ("dict size of kkk.kkk = ", (str(len(k_dict))))
+#print ("dict size of kkk.kkk = ", (str(len(k_dict))))
 
 v_dict = construct_dict_of_moves(v_file)
-print ("dict size of vvv.vvv = ", (str(len(v_dict))))
+#print ("dict size of vvv.vvv = ", (str(len(v_dict))))
 
+for key in v_dict.keys():
+    if key not in k_dict.keys():
+        print("------------------------")
+        print("Not in KKK....")
+        print(key)
+    count_v = v_dict[key]
+    count_k = 0;
+    if key in k_dict:
+        count_k = k_dict[key]
+    if count_v < count_k:
+        print("------------------------")
+        print("Extra " + str(count_k - count_v) + " in KKK....")
+        print(key)
+    if count_v > count_k:
+        print("------------------------")
+        print("Extra " + str(count_v - count_k) + " in VVV....")
+        print(key)
 
 
 
