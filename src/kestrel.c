940d0698a4b3933bafccf9f36fe920f5d94c0954
eddiemcnally <emcn@gmx.com>
2015-11-10 19:39:23 +0000

    Added a file omitted during the rename of 'main' to 'kestrel'

---
+++ src/kestrel.c
@@ -0,0 +1,110 @@
+/*
+ * kestrel.c
+ *
+ * ---------------------------------------------------------------------
+ * DESCRIPTION: main entry point for kestrel chess engine
+ * ---------------------------------------------------------------------
+ *
+ * Copyright (C) 2015 Eddie McNally <emcn@gmx.com>
+ *
+ * kestrel is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * kestrel is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include "types.h"
+#include "move.h"
+#include "board.h"
+#include "makemove.h"
+#include "board_utils.h"
+#include "hashkeys.h"
+#include "init.h"
+#include "occupancy_mask.h"
+#include "fen.h"
+#include "utils.h"
+#include "search.h"
+#include "move.h"
+
+// sample game position
+#define SAMPLE_POSITION 	"r1b1k2r/ppppnppp/2n2q2/2b5/3NP3/2P1B3/PP3PPP/RN1QKB1R w KQkq - 0 1"
+
+int main(int argc, char **argv)
+{
+	if (argc > 0) {
+		printf("%d", **argv);
+	}
+
+	// set process pri and cpu affinity for max performance
+	set_priority_and_affinity();
+
+	struct board *brd = init_game(SAMPLE_POSITION);
+	print_board(brd);
+
+	struct search_info si[1];
+
+////////////// debug /////////////////
+//	struct move_list mv_list = {
+//		.moves = {{0, 0}},
+//		.move_count = 0
+//	};
+
+//	generate_all_moves(brd, &mv_list);
+
+//	for (U16 i = 0; i < mv_list.move_count; i++) {
+//		printf("move %s\n", print_move(mv_list.moves[i].move_bitmap));
+//	}
+//	return 0;
+
+
+	si->depth = 4;
+	search_positions(brd, si);
+	if (si->depth >= 4)
+		return 0;
+/////////////////////////////////////////////
+
+	char input[6];
+	mv_bitmap move = NO_MOVE;
+	while (true) {
+		print_board(brd);
+		printf("Enter a move > ");
+		fgets(input, 6, stdin);
+
+		if (input[0] == 'q') {
+			// quit
+			break;
+		} else if (input[0] == 't') {
+			take_move(brd);
+		} else if (input[0] == 's'){
+			si->depth = 3;
+			search_positions(brd, si);
+
+		} else {
+			move = parse_move(input, brd);
+			if (move != NO_MOVE) {
+				make_move(brd, move);
+				if (is_repetition(brd)) {
+					printf("*** repetition ***\n");
+				}
+			} else {
+				printf("Move Not Parsed:%s\n", input);
+			}
+		}
+
+		fflush(stdin);
+	}
+
+	return 0;
+}
